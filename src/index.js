2ba9337889986b536f70b775ffff7744a3ee5dba
psorrells <pamelan.sorrells@gmail.com>
2022-06-02 19:12:43 -0500

    added express modules to index

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//NODE MODULES
+const express = require('express')
+const BodyParser = require('body-parser')
+
+
+const app = express()
+
+
+//render static html files in the public folder
+app.use(express.static('public'))
+//return middleware for urlencoded bodies
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
